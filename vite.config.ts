0c2358fef373e8d156a987ca3c6bdb9174a45b82
BORDKANONE <LMA709@yandex.ru>
2023-04-17 00:11:15 +0300

    fix: docker node version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       lib: resolve(__dirname, "src/lib"),
